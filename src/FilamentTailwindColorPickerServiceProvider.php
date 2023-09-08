17cce4a5fd891a9b7e56ddc7a5e8bfa7819bf3e2
Benny Haynes <48166490+OverboredThekid@users.noreply.github.com>
2023-09-08 11:09:32 -0500

    Update FilamentTailwindColorPickerServiceProvider.php

--- src/FilamentTailwindColorPickerServiceProvider.php
+++ src/FilamentTailwindColorPickerServiceProvider.php
@@ -16,11 +16,6 @@ class FilamentTailwindColorPickerServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name(static::$name)
             ->hasViews()
